cb6a35c450208fd5b48dfaac3a4a9b863673ebf5
yyounir <yyproton168@icloud.com>
2026-08-16 14:31:58 -0400

    0.1.2 - Updated files based on instructions

--- intropyproject-classify-pet-images/classify_images.py
+++ intropyproject-classify-pet-images/classify_images.py
@@ -65,4 +65,17 @@ def classify_images(images_dir, results_dic, model):
      Returns:
            None - results_dic is mutable data type so no return needed.         
     """
-    None 
+    for key in results_dic:
+        model_label = classifier(images_dir + key, model)
+        model_label = results_dic[key]
+
+        model_label = model_label.lower().strip()
+
+        # defines truth as pet image label
+        truth = results_dic[key][0]
+
+        if truth in model_label:
+            results_dic[key].extend([model_label, 1])
+
+        else:
+            results_dic[key].extend([model_label, 0])
